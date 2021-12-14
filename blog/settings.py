1080945a9d7d1cce5499df79ed8f561ad6fc8f36
Taylor <taylorpollioverturf@gmail.com>
2021-12-14 14:42:10 -0800

    Update project settings to include psql and api

--- blog/settings.py
+++ blog/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    # Our Apps
+    'api'
 ]
 
 MIDDLEWARE = [
@@ -75,11 +77,10 @@ WSGI_APPLICATION = 'blog.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'django-auth-practice',
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
